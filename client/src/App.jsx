e4f27a484a66ebde9760874261a49cddd4f3d160
ujjwalshri <128953832+ujjwalshri@users.noreply.github.com>
2024-04-18 22:33:15 +0530

    saved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 
 import {io} from 'socket.io-client';
-
+import { Container, TextField,Button, Typography } from '@mui/material';
 const App = () => {
    const socket = io('http://localhost:3000');
+   const [message, setMessage] = useState("");
+   const handleSubmit= (e)=>{
+        q.preventDefault();
+        socket.emit("message",message);
+        setMessage("");
 
+   }
     useEffect(()=>{
       socket.on("connect", ()=>{
         console.log("connected", socket.id);
@@ -19,9 +25,19 @@ const App = () => {
 
     },[])
   return (
-    <>
-    <Container/>
-    </>
+    
+    <Container maxWidth="sm">
+      <Typography variant="h1" component="div" gutterBottom>
+        Welcome to Socket.io
+      </Typography>
+    <form onSubmit={handleSubmit}> 
+      <TextField value={message} 
+      onChange={e=>setMessage(e.target.value)}
+      id="outlined-basic" label="Outlined" variant="outlined"/>
+      <Button type="submit" variant="contained" color="primary">Send</Button>
+    </form>
+     </Container>
+    
   )
 }
 
